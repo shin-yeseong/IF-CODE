5f81b31ba7da02c60e2b51937fe0ab1350e50078
yoos1eun <yoosieun@dgu.ac.kr>
2024-07-05 23:08:14 +0900

    Add files via upload
    1. 상단바 수정
     1) 메뉴바를 상단바에 넣는 것까진 했으나 클릭했을때 사이드바가 옆으로 나오게 하는 건 미완성. 사이드바가 나올 수 있도록 자바스크립트로 화면전환 하는 방법을 시도해봤으나 마무리 짓지 못함. "start.js"와 "category.css"가 그 흔적임.. 내일 예진이랑 협업해볼 예정!!><
     2) 상단바 센터부분은 로고 또는 이름 정해지면 이름 넣을 예정.
    
    2. 상단바 아래 연보라로 채워진 부분은 id="intro"는 웹사이트 소개 넣을 공간.
    
    3. 맨 아래 bottom부분은 웹사이트의 기본적인 정보들을 넣을 예정. 현재 developer 정보와 관련 사이트인 학과 공식 홈페이지, SNS링크 넣어둠. 웹사이트 로고를 넣어도 좋을 것 같음. 여기에 들어갈 부분은 계속 고민해볼 예정.
    
    4. 기존에 정한 색인 "6b24a1"이 생각보다 채도가 짙어 안 예쁨. 그래서 보라색 계열 채도 낮은 다양한 색을 사용함.
     1)6b24al,  2)874cb3, 3)ab90bf, 4)eee0f9 이렇게 4가지 섞어서 사용. 1에서 4로 갈수록 색이 옅어짐.

---
+++ start.js
@@ -0,0 +1,7 @@
+const main = document.querySelector("#main");
+const category = document.querySelector("#category");
+
+function begin(){
+    main.style.display="none";
+    category.style.display="block";
+}
